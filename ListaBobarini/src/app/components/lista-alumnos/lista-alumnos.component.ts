e698e0db0c142cbcfe9b721177534c140ccc10d9
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2022-09-27 23:20:05 -0300

    v5

--- ListaBobarini/src/app/components/lista-alumnos/lista-alumnos.component.ts
+++ ListaBobarini/src/app/components/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/models/student';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAlumnosComponent implements OnInit {
 
+listaEstudiantes: Array<Student> = [
+    {nombre: 'Kyle', apellido: 'Reese' , carrera: 'Angular', edad: 18, beca:true },
+    {nombre: 'John', apellido: 'Connor' , carrera:'Phyton', edad: 25, beca:false },
+    {nombre: 'Charlotte', apellido: 'Ferro' , carrera:'Phyton', edad: 20, beca:true },
+    {nombre: 'Sarah', apellido: 'Connor' , carrera: 'Angular', edad: 28, beca:false },
+];
+background = 'claro';
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  setBackground(estilo:string){
+    this.background=estilo;
+  }
 }
---
+++ ListaBobarini/src/app/models/student.ts
@@ -0,0 +1,7 @@
+export interface Student{
+    nombre: string;
+    apellido: string;
+    carrera: string;
+    edad: number;
+    beca: boolean;
+}
